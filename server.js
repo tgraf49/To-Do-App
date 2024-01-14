3ee9f6f860d6802eaacda6012d0c6eb7c7c6fe8c
Tyler Graf <tygraf13@gmail.com>
2024-01-14 11:19:20 -0600

    Set up Controllers, Models, and Views directories for future use

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-// require needed for modules to be used 
 const express = require('express')
-
-// initialize the app variable
 const app = express()
+require('dotenv').config()
 
-// create a homepage route
 app.get('/', (req, res) =>
 {
-// this gets sent to the client
-res.send("If my function is working I'll see this in the browser")
+    console.log("Somebody just hit the INDEX");
+    res.send("");
 });
 
-// form another route
 app.get('/second', (req, res) =>
 {
-res.send("My Second Page")
+    console.log("Second Page")
+    res.send("My Second Page")
 });
 
-// now you must call for the server to listen with app.listen() and give this function a port number
-// the callback function is optional to display text once the server comes online
-app.listen(3000, () => {
-    console.log('I am awake!')
-});
+app.listen(process.env.PORT)
